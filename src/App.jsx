4eef69b22b43333f38a9346df085b47cb6b82eba
rabab003 <bintaliraq4@gmail.com>
2025-05-19 21:16:41 +0300

    header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import sq from "./images/gradient.png"
+import Header from "./Components/Header"
 
 function App() {
   return (
     <>
     <main>
        {/* gradient image */}
-       <img src={sq}/>
+       <img src={sq} className="absolute top-0 right-0 opacity-60 -z-10"/>
+
+       {/* blur effect */}
+       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900_20px_#e99b63] -rotate-[30deg] -z-10">
+
+       </div>
+       <Header/>
     </main>
       
     </>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='flex items-center justify-between px-4 py-4 lg:px-20'>
+        <h1 className='m-0 text-3xl font-light md:text-4xl lg:text-5xl'>
+            MCODE
+
+        </h1>
+
+    </header>
+  )
+}
+
+export default Header
