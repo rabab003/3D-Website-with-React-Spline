2d789b8f0f8c2c25bf7bb2436aaa19cffe3ff248
rabab003 <bintaliraq4@gmail.com>
2025-05-19 21:02:15 +0300

    3D Website with React, Tailwind & Spline started

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import sq from "./images/gradient.png"
+
+function App() {
+  return (
+    <>
+    <main>
+       {/* gradient image */}
+       <img src={sq}/>
+    </main>
+      
+    </>
+  )
+}
+
+export default App
+
+// 10:30
